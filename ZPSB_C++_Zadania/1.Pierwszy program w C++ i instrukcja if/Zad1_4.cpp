c5e4c605cb8adae7d87645f536c9f2fb026492e4
WojciechA676 <wa30322@zpsb.pl>
2025-06-29 07:08:57 +0200

    Update Zad1_4.cpp

--- ZPSB_C++_Zadania/1.Pierwszy program w C++ i instrukcja if/Zad1_4.cpp
+++ ZPSB_C++_Zadania/1.Pierwszy program w C++ i instrukcja if/Zad1_4.cpp
@@ -16,10 +16,10 @@ int main() {
     std::string imie;
     int wiek;
 
-    std::cout << "Podaj swoje imię: ";
+    std::cout << "Podaj swoje imie: ";
     std::cin >> imie;
 
-    std::cout << "Podaj swój wiek: ";
+    std::cout << "Podaj swoj wiek: ";
     std::cin >> wiek;
 
     std::cout << "Witaj, " << imie << "! Masz " << wiek << " lat." << std::endl;
