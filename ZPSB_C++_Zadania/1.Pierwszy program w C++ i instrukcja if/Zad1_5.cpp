d937d9282aa23287d192b8e3f92953c45dc30943
WojciechA676 <wa30322@zpsb.pl>
2025-06-29 07:09:38 +0200

    Update Zad1_5.cpp

--- ZPSB_C++_Zadania/1.Pierwszy program w C++ i instrukcja if/Zad1_5.cpp
+++ ZPSB_C++_Zadania/1.Pierwszy program w C++ i instrukcja if/Zad1_5.cpp
@@ -18,21 +18,21 @@ Reszta z dzielenia: 2
 int main() {
     int a, b;
 
-    std::cout << "Podaj pierwszą liczbę: ";
+    std::cout << "Podaj pierwsza liczbe: ";
     std::cin >> a;
 
-    std::cout << "Podaj drugą liczbę: ";
+    std::cout << "Podaj druga liczbe: ";
     std::cin >> b;
 
     std::cout << "Suma: " << a + b << std::endl;
-    std::cout << "Różnica: " << a - b << std::endl;
+    std::cout << "Roznica: " << a - b << std::endl;
     std::cout << "Iloczyn: " << a * b << std::endl;
 
     if (b != 0) {
         std::cout << "Iloraz: " << a / b << std::endl;
         std::cout << "Reszta z dzielenia: " << a % b << std::endl;
     } else {
-        std::cout << "Nie można dzielić przez zero!" << std::endl;
+        std::cout << "Nie mozna dzielic przez zero!" << std::endl;
     }
 
     return 0;
